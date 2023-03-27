46dabd8e619e71858a4846bed529b7d7c4db4d33
sotiredofyours <h8everrything@gmail.com>
2023-03-27 14:49:30 +0400

    Graph: Add creating graph from CLI

---
+++ GraphCW/GraphCreator.cs
@@ -0,0 +1,22 @@
+﻿namespace GraphCW;
+
+public static class GraphCreator
+{
+    public static Graph CreateGraph(int nodes, int edges, IEnumerable<string> edgesCommands)
+    {
+        var graph = new Graph(nodes);
+        foreach (var con in edgesCommands)
+        {
+            var (from, to) = ParseConnection(con);
+            graph.Connect(from, to);
+        }
+        return graph;
+    }
+
+    private static (int, int) ParseConnection(string connection)
+    {
+        var parts = connection.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length < 3) throw new ArgumentException();
+        return (Convert.ToInt32(parts[0]), Convert.ToInt32(parts[2]));
+    }
+}
--- GraphCW/Program.cs
+++ GraphCW/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using GraphCW;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("New graph");
+Console.WriteLine("Enter the number of nodes:");
+var nodes = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the number of edges:");
+var edges = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter edges in format: <from node> - <to node>");
+var connections = new string[edges];
+for (int i = 0; i < edges; i++)
+{
+  connections[i] = Console.ReadLine()!;
+}
+var graph = GraphCreator.CreateGraph(nodes, edges, connections);
