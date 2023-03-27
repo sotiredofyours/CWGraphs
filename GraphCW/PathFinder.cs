07764f8d8c5d9788923f2cc5836d68848541e0b7
sotiredofyours <h8everrything@gmail.com>
2023-03-27 16:25:01 +0400

    Path finder: implement parallel algorithm

---
+++ GraphCW/PathFinder.cs
@@ -0,0 +1,24 @@
+﻿namespace GraphCW;
+
+public static class PathFinder
+{
+    private static volatile int  _maxPath = 0;
+    private static object obj = new object();
+    
+    public static int FindLongestPath(Graph graph)
+    {
+        Parallel.ForEach(graph.Nodes, node => FindPath(node, 0, null, new List<Node>()));
+        return _maxPath;
+    }
+
+    private static void FindPath(Node currentNode, int currentPath, Node previous, List<Node> used)
+    {
+        if (currentPath > _maxPath)
+        { 
+            _maxPath = currentPath;
+        }
+        used.Add(currentNode);
+        var possiblePaths = currentNode.IncidentNodes.Where(x => x != previous && !used.Contains(x));
+        Parallel.ForEach(possiblePaths, node => FindPath(node, currentPath + 1, node, used));
+    }
+}
--- GraphCW/Program.cs
+++ GraphCW/Program.cs
@@ -5,10 +5,11 @@ Console.WriteLine("Enter the number of nodes:");
 var nodes = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Enter the number of edges:");
 var edges = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Enter edges in format: <from node> - <to node>");
+Console.WriteLine("Enter edges in format: <from node> - <to node>. NOTE: nodes numbers in range [0..nodes.Length - 1]");
 var connections = new string[edges];
 for (int i = 0; i < edges; i++)
 {
   connections[i] = Console.ReadLine()!;
 }
 var graph = GraphCreator.CreateGraph(nodes, edges, connections);
+Console.WriteLine(PathFinder.FindLongestPath(graph));
